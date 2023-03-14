e5994babdac84de018927ba0f47226a0bca4166c
natnael <natiyeshi16@.com>
2023-03-14 10:21:06 +0300

    edited F

--- public/js/deller.js
+++ public/js/deller.js
@@ -4,19 +4,17 @@ function fillColumn() {
     for(i = 0; i < 80; i++){
         columns.innerHTML += `
         <div class="row ">
-                <div class="col b-box">. . . .</div>
-                <div class="col i-box">. . . .</div>
-                <div class="col n-box">. . . .</div>
-                <div class="col g-box">. . . .</div>
-                <div class="col o-box">. . . .</div>
+                <div class="col b-box" id="b${i}">. . . .</div>
+                <div class="col i-box" id="i${i}">. . . .</div>
+                <div class="col n-box" id="n${i}">. . . .</div>
+                <div class="col g-box" id="g${i}">. . . .</div>
+                <div class="col o-box" id="o${i}">. . . .</div>
         </div>
         ` 
     }   
 }
 
 fillColumn()
-
-
 
 let forms = document.querySelectorAll("form")
 forms.forEach(element => {
@@ -98,41 +96,78 @@ let bc = ic = nc = gc = oc = 0
 }
 
 setBetMessage()
+let jump = true
+
+function pause(){
+    jump = !jump
+    if(!jump) currentValue.classList.remove("circleIt")
+    else currentValue.classList.add("circleIt")
+    
+}
 
 function start() {
     setBetMessage()
-    buttons.innerHTML = `<button class="start bg-success  text-white" onclick="beingo()">beingo</button>`
+    buttons.innerHTML = `
+    <button class="start bg-success  text-white" onclick="beingo()">beingo</button>
+    <button class="start bg-danger  text-white" onclick="pause()" >pause</button>
+    `
     let getShuffledFile = shuffle()
     currentValue.classList.add("circleIt")
     let counter = 0
     intervalId = setInterval(() => {
+        if(jump == false){
+            return
+        }
         let item = getShuffledFile[counter++]
         if(item[0] == "b"){
             bcol[bc].innerHTML = item
+            location.href = "#"+bcol[bc].id
             bcol[bc++].classList.add("checked") 
         }
         else if(item[0] == "i"){
             icol[ic].innerHTML = item
+            location.href = "#"+ icol[ic].id
             icol[ic++].classList.add("checked") 
         }
         else if(item[0] == "n"){
             ncol[nc].innerHTML = item
+            location.href = "#"+ ncol[nc].id
             ncol[nc++].classList.add("checked") 
         }
         else if(item[0] == "g"){
             gcol[gc].innerHTML = item
+            location.href = "#"+ gcol[gc].id
             gcol[gc++].classList.add("checked") 
         }
         else if(item[0] == "o"){
             ocol[oc].innerHTML = item
+            location.href = "#"+ ocol[oc].id
             ocol[oc++].classList.add("checked") 
         }
         currentValue.innerHTML = item
+        setPreviouseValue(item)
     }, 3000);
 }
+let preValues = []
+function setPreviouseValue(value){
+    preValues.splice(0,0,value)
+    if(preValues.length > 3){
+        preValues.pop()
+    }
+    a = 1
+    for(i of preValues){
+        document.querySelector(".last"+a).innerHTML = i
+        a++
+    }
+}
+
 
 function beingo() {
-
+    preValues = []
+    jump = true
+    for(i = 1; i < 4; i++){
+        document.querySelector(".last"+i).innerHTML = "..."
+    }
     clearInterval(intervalId)
     currentValue.classList.remove("circleIt")
     buttons.innerHTML = `<button class="start bg-primary  text-white" onclick="restart()">Restart</button>`
